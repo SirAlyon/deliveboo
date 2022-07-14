30485dfcb248be539812a1a94facd2d455f2e61d
FraAccaBi <francescahuda@gmail.com>
2022-07-14 16:15:45 +0200

    close #3; import-db

---
+++ config/db.php
@@ -0,0 +1,123 @@
+<?php
+
+return [
+    'types' => [
+        [
+            'name' => 'Cinese',
+            'img' => 'https://images.pexels.com/photos/2347311/pexels-photo-2347311.jpeg?cs=srgb&dl=pexels-engin-akyurt-2347311.jpg&fm=jpg',
+
+        ],
+        [
+            'name' => 'Gelateria',
+            'img' => 'https://images.pexels.com/photos/5570880/pexels-photo-5570880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        ],
+        [
+            'name' => 'Giapponese',
+            'img' => 'https://images.pexels.com/photos/3763816/pexels-photo-3763816.jpeg?cs=srgb&dl=pexels-horizon-content-3763816.jpg&fm=jpg',
+        ],
+        [
+            'name' => 'Indiano',
+            'img' => 'https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?cs=srgb&dl=pexels-chan-walrus-958545.jpg&fm=jpg',
+        ],
+        [
+            'name' => 'Internazionale',
+            'img' => 'https://images.pexels.com/photos/941861/pexels-photo-941861.jpeg?cs=srgb&dl=pexels-chan-walrus-941861.jpg&fm=jpg',
+        ],
+        [
+            'name' => 'Italiano',
+            'img' => 'https://images.pexels.com/photos/1566837/pexels-photo-1566837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        ],
+        [
+            'name' => 'Messicano',
+            'img' => 'https://images.pexels.com/photos/7772198/pexels-photo-7772198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        ],
+        [
+            'name' => 'Pizzeria',
+            'img' => 'https://images.pexels.com/photos/905847/pexels-photo-905847.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        ],
+        [
+            'name' => 'Street food',
+            'img' => 'https://images.pexels.com/photos/920570/pexels-photo-920570.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        ],
+        [
+            'name' => 'Vegetariano',
+            'img' => 'https://images.pexels.com/photos/6608617/pexels-photo-6608617.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        ],
+
+
+    ],
+
+    'products' => [
+        [
+            'name' => 'Antipasti caldi',
+            'description' => 'Lorem ipsum dolor',
+            'price' => '10.99',
+            'visibility' => 'TRUE',
+            'img' => 'https://images.pexels.com/photos/1267320/pexels-photo-1267320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        ],
+        [
+            'name' => 'Antipasti freddi',
+            'description' => 'Lorem ipsum dolor',
+            'price' => '9.99',
+            'visibility' => 'TRUE',
+            'img' => 'https://images.pexels.com/photos/59524/pexels-photo-59524.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        ],
+        [
+            'name' => 'Pasta allo scoglio',
+            'description' => 'Lorem ipsum dolor',
+            'price' => '12.80',
+            'visibility' => 'TRUE',
+            'img' => 'https://images.pexels.com/photos/921374/pexels-photo-921374.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        ],
+        [
+            'name' => 'Risotto',
+            'description' => 'Lorem ipsum dolor',
+            'price' => '15.60',
+            'visibility' => 'TRUE',
+            'img' => 'https://images.pexels.com/photos/2067418/pexels-photo-2067418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        ],
+        [
+            'name' => 'Braciole',
+            'description' => 'Lorem ipsum dolor',
+            'price' => '15.00',
+            'visibility' => 'TRUE',
+            'img' => 'https://images.pexels.com/photos/11749407/pexels-photo-11749407.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        ],
+        [
+            'name' => 'Verdure in padella',
+            'description' => 'Lorem ipsum dolor',
+            'price' => '7.50',
+            'visibility' => 'TRUE',
+            'img' => 'https://images.pexels.com/photos/11213759/pexels-photo-11213759.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        ],
+        [
+            'name' => 'Tiramisù',
+            'description' => 'Lorem ipsum dolor',
+            'price' => '6.00',
+            'visibility' => 'TRUE',
+            'img' => 'https://images.pexels.com/photos/6328588/pexels-photo-6328588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        ],
+        [
+            'name' => 'Gelato',
+            'description' => 'Lorem ipsum dolor',
+            'price' => '6.00',
+            'visibility' => 'TRUE',
+            'img' => 'https://images.pexels.com/photos/1362534/pexels-photo-1362534.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        ],
+        [
+            'name' => 'Coca Cola',
+            'description' => 'Lorem ipsum dolor',
+            'price' => '3.00',
+            'visibility' => 'TRUE',
+            'img' => 'https://images.pexels.com/photos/50593/coca-cola-cold-drink-soft-drink-coke-50593.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        ],
+        [
+            'name' => 'Birra',
+            'description' => 'Lorem ipsum dolor',
+            'price' => '4.00',
+            'visibility' => 'TRUE',
+            'img' => 'https://images.pexels.com/photos/1672304/pexels-photo-1672304.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        ]
+    ]
+
+];
