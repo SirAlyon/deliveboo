e66126effef8f38ce4fa71fefcebe79bfc6c3047
Alexxboc <boccardi.alessandro@gmail.com>
2022-07-15 14:41:13 +0200

    Create show registration method

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use App\Models\Type;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -23,6 +24,13 @@ class RegisterController extends Controller
     */
 
     use RegistersUsers;
+
+    public function showRegistrationForm()
+    {
+        $types = Type::all();
+        //dd($types);
+        return view('auth.register', compact('types'));
+    }
 
     /**
      * Where to redirect users after registration.
