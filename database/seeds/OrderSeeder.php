605db2f25c3038dd412fba3006cc33de6a758436
SirAlyon <alessiosada04@gmail.com>
2022-07-15 20:42:33 +0200

    add order index.blade

--- database/seeds/OrderSeeder.php
+++ database/seeds/OrderSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Models\Order;
 use Illuminate\Database\Seeder;
-
 class OrderSeeder extends Seeder
 {
     /**
@@ -11,6 +11,18 @@ class OrderSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $orders = config('db.orders');
+
+        foreach ($orders as $order) {
+            $new_order = new Order();
+            $new_order->guest_name = $order['guest_name'];
+            $new_order->guest_lastname = $order['guest_lastname'];
+            $new_order->guest_email = $order['guest_email'];
+            $new_order->guest_address = $order['guest_address'];
+            $new_order->guest_phone_number = $order['guest_phone_number'];
+            $new_order->total_price = $order['total_price'];
+            $new_order->save();
+        }
     }
 }
+
