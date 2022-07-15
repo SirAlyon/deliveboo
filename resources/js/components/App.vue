3bc3c354bbd741136bd7c30ff7b7dbe41feb6af1
FraAccaBi <francescahuda@gmail.com>
2022-07-15 10:26:34 +0200

    Install Vue Router Create guest extended layout Create App.vue main component Create front.js file Create router.js file Add front-end routes Test guest pages >>Create Home/Restaurant/Checkout pages

---
+++ resources/js/components/App.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <router-link class="navbar-brand" :to="{name: 'home'}">
+                <h1>Boolpress</h1>
+            </router-link>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item" v-for="item in menu_items" :key="item">
+                        <router-link :to="{name: item.route_name}">
+                            {{item.route_text}}
+                        </router-link>
+                    </li>
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+
+
+            </div>
+        </div>
+    </nav>
+
+        <router-view></router-view>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'App',
+        data(){
+            return{
+                menu_items:[
+                    {
+                        route_name: 'home',
+                        route_text: 'Home'
+                    },
+                    /* {
+                        route_name: 'about',
+                        route_text: 'About'
+                    },
+                    {
+                        route_name: 'posts',
+                        route_text: 'Posts'
+                    } */
+                ]
+            }
+        }
+    }
+</script>
