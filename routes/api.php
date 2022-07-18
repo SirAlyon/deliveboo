36cb175e0c19d8866f6b466ab51fd8fb1b02d783
Alexxboc <boccardi.alessandro@gmail.com>
2022-07-18 15:42:58 +0200

    Create types api route

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('restaurants', 'API\RestaurantController@index');
+Route::get('types', 'API\TypesController@index');
+
+
