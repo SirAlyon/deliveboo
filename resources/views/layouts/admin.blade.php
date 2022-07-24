2cb9a4275360cc05ddd190ff3ddf5f05d5d5e9a8
FraAccaBi <francescahuda@gmail.com>
2022-07-24 13:05:59 +0200

    fix dashboard duplicate

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -95,44 +95,7 @@
                 </div>
             </div>
 
-        <div class="container-fluid">
-            <div class="row">
-                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-side sidebar collapse">
-                    <div class="position-sticky pt-3">
-                        <ul class="nav flex-column">
-                            <li class="nav-item" class="{{ request()->routeIs('admin.*') ? 'active' : ''}}">
-                                <a class="nav-link" href="{{route('admin.dashboard')}}">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon-dash" width="20" height="20" fill="currentColor" class="bi bi-speedometer2" viewBox="0 0 16 16">
-                                <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"/>
-                                <path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z"/>
-                                </svg>
-                                   <span class="font-side">Dashboard</span>
-                                </a>
-                            </li>
-                            <li class="nav-item {{ Route::is('admin.products') ? 'active' : '' }}">
-                                <a class="nav-link" href="{{route('admin.products.index')}}">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon-dash" width="20" height="20" viewBox="0 0 26 26" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart" aria-hidden="true"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
-                                    <span class="font-side">Products</span>
-                                </a>
-                            </li>
-                            <li class="nav-item {{Route::currentRouteName() === 'orders' ? 'active' : '' }}">
-                                <a class="nav-link" href="{{route('admin.orders.index')}}">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon-dash"  width="20" height="20" viewBox="0 0 26 26" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file" aria-hidden="true"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
-                                   <span class="font-side">Orders</span>
-                                </a>
-                            </li>
 
-                        </ul>
-
-
-                    </div>
-                </nav>
-
-                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                    @yield('content')
-                </main>
-            </div>
-        </div>
     </body>
 
 </html>
