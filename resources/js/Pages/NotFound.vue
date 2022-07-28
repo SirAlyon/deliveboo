11ee68b599122b770b528ebb57516d9966de18e3
nehabht <ne.bhatti2@icloud.com>
2022-07-28 17:47:53 +0200

    closes #144;page notfound

--- resources/js/Pages/NotFound.vue
+++ resources/js/Pages/NotFound.vue
@@ -1,33 +1,60 @@
 <template>
-    <div class="notfound_page">
-        <div class="p-5 bg-light">
-            <div class="container d-flex justify-content-between">
-                <div class="ms-3">
-                    <h1 class="display-3">Ops! Page not found, 404</h1>
+  <div class="notfound_page">
+    <div class="container section_wrapper">
+      <h1 class="display-3">Page not found</h1>
 
-                </div>
-                <img src="https://picsum.photos/200" alt="">
+      <div class="container px-4">
+        <span>
+            This page doesn't exist.
+        </span>
+        <br>
+        <div class="btn btn-primary text-decoration-none mt-4">
+            <router-link :to="{ name: 'home' }">Go Home</router-link>
 
-            </div>
-
-            <div class="container px-4">
-                This page doesn't exist. Go back to:
-                    <router-link :to="{name:'home'}">HomePage</router-link>
-            </div>
         </div>
+      </div>
     </div>
-
+  </div>
 </template>
 
 <script>
 export default {
-   neme: 'NotFound'
-}
+  neme: "NotFound",
+};
 </script>
 
 
 <style lang="scss" scoped>
-.notfound_page{
-    min-height: 100vh;
+.notfound_page {
+  min-height: 100vh;
+  background-image: url(https://deliveroo.it/assets/images/error-pages/background-7edaa013aceff22965bdb7fbf274b4a2.svg);
+  background-repeat: no-repeat;
+  text-align: center;
+  padding: 137px 16px 32px;
+}
+
+.section_wrapper {
+  background: transparent
+    url(https://deliveroo.it/assets/images/error-pages/roo-5c784b3ea96399e6269af8a11ec6349c.svg) no-repeat 50% 0;
+    padding-top: 200px;
+
+    span{
+        color:rgb(27, 27, 27)
+    }
+
+    .btn.btn-primary {
+    background-repeat: no-repeat;
+    background-color: #00ccbc;
+    border: none;
+    box-shadow: none;
+    outline: none;
+    color: #FFF;
+    font-weight: 600;
+    }
+
+    a {
+    color: white;
+    text-decoration: none;
+    }
 }
 </style>
